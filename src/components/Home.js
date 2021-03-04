ec383999c463bed2562dcb813b67d98f387c9881
mahesh-muttinti <maheshmuttinti@gmail.com>
2021-03-04 21:15:32 +0530

    modified the files

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react"
 
 import ResaleTicketIcon from '../assets/body/resale.svg'
+import BuyOurTicket from '../assets/body/buy_our_ticket.svg'
+import GetOurTicket from '../assets/body/getour_ticket.svg'
+import NetflixIcon from '../assets/body/netflex.svg'
+import PrimeVideoIcon from '../assets/body/prime_video.svg'
+import BookMyShowIcon from '../assets/body/bookmyshow.svg'
+import AhaIcon from '../assets/body/aha.svg'
 class Home extends Component {
   render() {
     return (
@@ -44,7 +50,7 @@ class Home extends Component {
                     </p>
                 </div>
                 <div className="steps step-two">
-                    <img src={ResaleTicketIcon} alt="buy our ticket" />
+                    <img src={BuyOurTicket} alt="buy our ticket" />
                     <p className="title buy-ticket-title">
                         Buy our ticket
                     </p>
@@ -53,7 +59,7 @@ class Home extends Component {
                     </p>
                 </div>
                 <div className="steps step-three">
-                    <img src={ResaleTicketIcon} alt="get your ticket" />
+                    <img src={GetOurTicket} alt="get your ticket" />
                     <p className="title get-ticket-title">
                         Get your ticket
                     </p>
@@ -67,114 +73,116 @@ class Home extends Component {
             <div className="movie-heading">
                 <h1 className="__h1">Movies</h1>
             </div>
-            <div className="movies_container">
-                <div className="movie1 movie-cards-container">
-                    <div className="img-movie">
-                        <img src="https://d1ydle56j7f53e.cloudfront.net/assets/gallery/movie/chakra-13939/misc-big/chakra-2.jpg" alt="movie img" />
-                        <div className="rating">
-                            
-                        </div>
-                        <div className="movie-details">
-                            <h2 className="movie-name">Chakra</h2>
-                            <p className="status">UA</p>
-                            <p className="languages">Telugu,Tamil,Kannada</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="movie2 movie-cards-container">
-                    <div className="img-movie">
-                        <img src="https://content.tupaki.com/twdata/2020/0620/photos/movies/Allari%20Naresh%20Nandi%20Character%20Introduction%20posters/normal/Allari%20Naresh%20Nandi%20Character%20Introduction%20posters_2.jpg" alt="movie img" />
-                        <div className="rating">
-                            
-                        </div>
-                        <div className="movie-details">
-                            <h2 className="movie-name">Naandhi</h2>
-                            <p className="status">UA</p>
-                            <p className="languages">Telugu</p>
+            <div className="mcontainer">
+                <div className="movies_container">
+                    <div className="movie1 movie-cards-container">
+                        <div className="img-movie">
+                            <img src="https://d1ydle56j7f53e.cloudfront.net/assets/gallery/movie/chakra-13939/misc-big/chakra-2.jpg" alt="movie img" />
+                            <div className="rating">
+                                
+                            </div>
+                            <div className="movie-details">
+                                <h2 className="movie-name">Chakra</h2>
+                                <p className="status">UA</p>
+                                <p className="languages">Telugu,Tamil,Kannada</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="movie3 movie-cards-container">
-                    <div className="img-movie">
-                        <img src="https://www.seelatest.com/images/poster/zombie-reddy.jpg" alt="movie img" />
-                        <div className="rating">
-                            
-                        </div>
-                        <div className="movie-details">
-                            <h2 className="movie-name">Zombie Reddy</h2>
-                            <p className="status">UA</p>
-                            <p className="languages">Telugu</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="movie4 movie-cards-container">
-                    <div className="img-movie">
-                        <img src="https://animatedviews.com/wp-content/uploads/2020/12/t-and-j-1.jpg" alt="movie img" />
-                        <div className="rating">
-                            
-                        </div>
-                        <div className="movie-details">
-                            <h2 className="movie-name">Tom and Jerry</h2>
-                            <p className="status">UA</p>
-                            <p className="languages">Telugu,English</p>
+                    <div className="movie2 movie-cards-container">
+                        <div className="img-movie">
+                            <img src="https://content.tupaki.com/twdata/2020/0620/photos/movies/Allari%20Naresh%20Nandi%20Character%20Introduction%20posters/normal/Allari%20Naresh%20Nandi%20Character%20Introduction%20posters_2.jpg" alt="movie img" />
+                            <div className="rating">
+                                
+                            </div>
+                            <div className="movie-details">
+                                <h2 className="movie-name">Naandhi</h2>
+                                <p className="status">UA</p>
+                                <p className="languages">Telugu</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="movie5 movie-cards-container">
-                    <div className="img-movie">
-                        <img src="https://cdn.gulte.com/wp-content/uploads/2020/08/Sumanth.jpg" alt="movie img" />
-                        <div className="rating">
-                            
-                        </div>
-                        <div className="movie-details">
-                            <h2 className="movie-name">Kapatadhari</h2>
-                            <p className="status">UA</p>
-                            <p className="languages">Telugu,English</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="movie6 movie-cards-container">
-                    <div className="img-movie">
-                        <img src="https://indianmovieradar.com/wp-content/uploads/2020/02/Uppena-Poster-3.jpg" alt="movie img" />
-                        <div className="rating">
-                            
-                        </div>
-                        <div className="movie-details">
-                            <h2 className="movie-name">Uppena</h2>
-                            <p className="status">UA</p>
-                            <p className="languages">Telugu</p>
+                    <div className="movie3 movie-cards-container">
+                        <div className="img-movie">
+                            <img src="https://www.seelatest.com/images/poster/zombie-reddy.jpg" alt="movie img" />
+                            <div className="rating">
+                                
+                            </div>
+                            <div className="movie-details">
+                                <h2 className="movie-name">Zombie Reddy</h2>
+                                <p className="status">UA</p>
+                                <p className="languages">Telugu</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="movie7 movie-cards-container">
-                    <div className="img-movie">
-                        <img src="https://1.bp.blogspot.com/-uPERbFZSDms/XiBZVq5VUYI/AAAAAAAAQFA/UMoo8OhZL7ULN5w8GfROSH3divF94qQCACLcBGAsYHQ/s1600/Krack-telugu-movie-star-cast-release-date-poster.jpg" alt="movie img" />
-                        <div className="rating">
-                            
+                    <div className="movie4 movie-cards-container">
+                        <div className="img-movie">
+                            <img src="https://animatedviews.com/wp-content/uploads/2020/12/t-and-j-1.jpg" alt="movie img" />
+                            <div className="rating">
+                                
+                            </div>
+                            <div className="movie-details">
+                                <h2 className="movie-name">Tom and Jerry</h2>
+                                <p className="status">UA</p>
+                                <p className="languages">Telugu,English</p>
+                            </div>
                         </div>
-                        <div className="movie-details">
-                            <h2 className="movie-name">Krack</h2>
-                            <p className="status">UA</p>
-                            <p className="languages">Telugu</p>
+                    </div>
+                    <div className="movie5 movie-cards-container">
+                        <div className="img-movie">
+                            <img src="https://cdn.gulte.com/wp-content/uploads/2020/08/Sumanth.jpg" alt="movie img" />
+                            <div className="rating">
+                                
+                            </div>
+                            <div className="movie-details">
+                                <h2 className="movie-name">Kapatadhari</h2>
+                                <p className="status">UA</p>
+                                <p className="languages">Telugu,English</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="movie6 movie-cards-container">
+                        <div className="img-movie">
+                            <img src="https://indianmovieradar.com/wp-content/uploads/2020/02/Uppena-Poster-3.jpg" alt="movie img" />
+                            <div className="rating">
+                                
+                            </div>
+                            <div className="movie-details">
+                                <h2 className="movie-name">Uppena</h2>
+                                <p className="status">UA</p>
+                                <p className="languages">Telugu</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="movie7 movie-cards-container">
+                        <div className="img-movie">
+                            <img src="https://1.bp.blogspot.com/-uPERbFZSDms/XiBZVq5VUYI/AAAAAAAAQFA/UMoo8OhZL7ULN5w8GfROSH3divF94qQCACLcBGAsYHQ/s1600/Krack-telugu-movie-star-cast-release-date-poster.jpg" alt="movie img" />
+                            <div className="rating">
+                                
+                            </div>
+                            <div className="movie-details">
+                                <h2 className="movie-name">Krack</h2>
+                                <p className="status">UA</p>
+                                <p className="languages">Telugu</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
          </section>
         <section className="partners-container">
-            <h2 className="__h1 move_to_margin">Partners</h2>
+            <h2 className="__h1 partners-heading">Partners</h2>
             <div className="logos-container">
                 <div className="logo_p logo1">
-                    <img src="http://getlogo.net/wp-content/uploads/2020/04/bookmyshow-logo-vector.png" alt="partners avatars" />
+                    <img src={BookMyShowIcon} alt="partners avatars" />
                 </div>
                 <div className="logo_p logo2">
-                    <img src="https://img01.products.bt.co.uk/content/dam/bt/portal/images/logos/tv/Prime-Video-Color.jpg" alt="partners avatars" />
+                    <img src={PrimeVideoIcon} alt="partners avatars" />
                 </div>
                 <div className="logo_p logo3">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1280px-Netflix_2015_logo.svg.png" alt="partners avatars" />
+                    <img src={NetflixIcon} alt="partners avatars" />
                 </div>
                 <div className="logo_p logo4">
-                    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/f/fb/Aha_OTT_Logo.jpeg/170px-Aha_OTT_Logo.jpeg" alt="partners avatars" />
+                    <img src={AhaIcon} alt="partners avatars" />
                 </div>
             </div>
         </section>
