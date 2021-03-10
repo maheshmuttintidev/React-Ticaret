7bac2a3b24c270dcc4aa71e07229bf2665eb23b7
mahesh-muttinti <maheshmuttinti@gmail.com>
2021-03-10 17:56:04 +0530

    organise the components in a proper manner, added the login popup and made the changes in design

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,11 @@
 import React, { Component, Suspense } from "react"; 
+import Home from './Home';
 const ProfileMenu = React.lazy(() => import('./ProfileMenu'))
 class Profile extends Component {
   render() {
     return (
       <main>
+        <Home />
         <Suspense fallback={
           <div className="loader-1">
             <div className="loading-child"></div>
