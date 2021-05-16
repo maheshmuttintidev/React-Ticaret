e65604b811c67bfb3b33431c88d4b7fc8073411e
mahesh-muttinti <maheshmuttinti@gmail.com>
2021-05-16 15:16:50 +0530

    made some changes

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import { LOGIN_USER, REGISTER_USER } from '../constants/user.constants'
 
 export const login = async (user) => {
     try {
-        const responseData = await axios.post('http://localhost:5000/user/login', user)
+        const responseData = await axios.post('https://ticaret-001.herokuapp.com/user/login', user)
         return {
             type: LOGIN_USER,
             payload: responseData
@@ -15,7 +15,7 @@ export const login = async (user) => {
 
 export const register = async (user) => {
     try {
-        const responseData = await axios.post('http://localhost:5000/user/register', user)
+        const responseData = await axios.post('https://ticaret-001.herokuapp.com/user/register', user)
         return {
             type: REGISTER_USER,
             payload: responseData
