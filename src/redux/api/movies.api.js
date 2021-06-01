a44d3936620f156b855397cf4b58987cbf8551d6
mahesh-muttinti <maheshmuttinti@gmail.com>
2021-06-01 20:27:39 +0530

    some changes happened

--- src/redux/api/movies.api.js
+++ src/redux/api/movies.api.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
-import { BASE_URI_PRODUCTION } from './uris'
+import { BASE_URI_DEV } from './uris'
 
 export const getMoviesResponse = async () => {
     try {
-        return axios.get(`${BASE_URI_PRODUCTION}movies-list`)
+        return axios.get(`${BASE_URI_DEV}movies-list`)
     } catch (e) {
         console.log(e)
     }
